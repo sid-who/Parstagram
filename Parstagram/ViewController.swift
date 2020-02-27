c97b1ea8b994be6c20b51952648ed69a228adcd9
Gurpreet Sidhu <gsidhu@csumb.edu>
2020-02-26 17:01:03 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Gurpreet Sidhu on 2/26/20.
+//  Copyright © 2020 gsidhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
